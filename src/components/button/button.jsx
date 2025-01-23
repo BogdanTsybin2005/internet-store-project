2724fb6716d1ed6847356929da1dc24d00a158bf
BogdanTsybin2005 <tsybinb@gmail.com>
2025-01-23 14:12:42 +0600

    Сделал кнопу к каждой карточки для добавления товара в определенном количестве, которое пользователь может сам для себя задать.

---
+++ src/components/button/button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './button.css';
+
+
+
+export default function AddToCardButton(props) {
+    const {buttonText, buttonAddCounter, onIncrease, onDecrease} = props;
+    return <> 
+        {
+            buttonAddCounter > 0 
+            ? 
+                <div className="meter-controller-button">
+                    <button onClick={onDecrease} className="decrease-value">-</button>
+                    <p className="counter-value">{buttonAddCounter}</p>
+                    <button onClick={onIncrease} className="increase-value">+</button>
+                </div> 
+            : 
+                <button className="base-button">{buttonText}</button>
+        }
+    </>
+}
---
+++ src/components/button/container.jsx
@@ -0,0 +1,25 @@
+import AddToCardButton from "./button";
+import { useState } from "react";
+import React from "react";
+
+
+export default function ButtonContainer() {
+    const [buttonPropCounter, setButtonPropCounter] = useState(0);
+
+    const increaseCounterValue = () => {
+        setButtonPropCounter(buttonPropCounter => buttonPropCounter + 1);
+    }
+
+    const decreaseCounterValue = () => {
+        if (buttonPropCounter > 0) {
+            setButtonPropCounter(buttonPropCounter => buttonPropCounter - 1);
+        }
+    }
+
+    return <AddToCardButton 
+        buttonText='Add to card' 
+        buttonAddCounter={buttonPropCounter} 
+        onIncrease={increaseCounterValue}
+        onDecrease={decreaseCounterValue}
+    />
+}
